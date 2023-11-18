da88536de3bca3c3eab58d32733f0c79203608ff
aansd <septiyan63@gmail.com>
2023-11-18 21:22:13 +0700

    Develop TaskList View

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="{{ asset('style.css') }}">
+  <title>TaskList</title>
+</head>
+
+<body>
+  <div class="task-list-container">
+    <h1 class="task-list-heading">Task List</h1>
+
+    <div class="task-list-table-head">
+      <div class="task-list-header-task-name">Task Name</div>
+      <div class="task-list-header-detail">Detail</div>
+      <div class="task-list-header-due-date">Due Date</div>
+      <div class="task-list-header-progress">Progress</div>
+    </div>
+
+
+    @foreach ($tasks as $item)
+        
+    <div class="table-body">
+      <div class="table-body-task-name">
+        <span class="material-icons @if ($item->status == 'completed') check-icon-completed @else check-icon @endif" >
+          check_circle
+        </span>
+        {{$item->name}}
+      </div>
+      <div class="table-body-detail">{{$item->detail}}</div>
+      <div class="table-body-due-date">{{$item->due_date}}</div>
+      <div class="table-body-progress">
+        @switch($item->status)
+            @case('in_progress')
+                In Progress
+                @break
+            @case('in_review')
+                Waiting/In Review
+                @break
+            @case('completed')
+                Complated
+            @default
+                Not Started
+        @endswitch
+      </div>
+    </div>
+    @endforeach
+  </div>
+</body>
+
+</html>
